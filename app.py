540ae3d8d01179e890520e87c0fcbe9499eb4681
Prince Sharma <124443333+PrinceCharles12@users.noreply.github.com>
2026-03-19 20:11:01 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 
